c0817a5edc427053d064f4e09cfadf846cbbca7c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-05-17 22:23:26 +0000

    updated lambda/languageStrings.js.

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -11,11 +11,10 @@ module.exports = {
             WELCOME_MSG: `Olá! Bem vindo à sua meta diária. Quais são seus planos para hoje?`,
             WELCOME_REPROMPT_MSG: `Meu plano para hoje é ficar aqui na minha caixinha. Quais são os seus?`,
             WELCOME_BACK_MSG: `Hm deixa eu ver aqui. Parece que suas metas para hoje são {{goals}}.`,
-            WELCOME_BACK_MSG_plural: `Que bom que você está de volta! Parece que faltam {{count}} dias até o seu {{age}}º aniversário.`,
             NO_GOALS_MSG: `Parece que você não tem metas para hoje!`,
             REGISTER_GOALS_MSG: `Obrigada, eu vou lembrar que suas metas para hoje são {{goals}}.`,
-            HELP_MSG: `Você pode me dizer o dia em que você nasceu e eu vou tomar nota. Você também pode dizer, "anote meu aniversário" eu irei guiar sua interação. Qual deles você quer tentar?`,
-            GOODBYE_MSG: `Tchau!`,
+            HELP_MSG: `Você pode me dizer quais são as atividades que quer realizar hoje. Eu vou guardar e você pode verificar comigo ao longo do dia!`,
+            GOODBYE_MSG: `Tchau! Siga firme nas suas metas!`,
             REFLECTOR_MSG: `Você acabou de acionar o {{intentName}}`,
             ERROR_MSG: `Desculpe, eu não entendi o que você disse. Pode reformular?`,
             ERROR_TIMEZONE_MSG: `Não consegui determinar seu fuso horário. Por favor verifique as configurações de seu aparelho e certifique-se de que um fuso horário está selecionado. Depois disso, reinicie a skill e tente novamente!`
