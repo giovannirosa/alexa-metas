633d7729b3a3f99a0a508b3bca8dabbc514411ca
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-05-18 03:27:31 +0000

    updated lambda/languageStrings.js.

--- lambda/languageStrings.js
+++ lambda/languageStrings.js
@@ -11,6 +11,7 @@ module.exports = {
             WELCOME_MSG: `Olá! Bem vindo à sua meta diária. Quais são seus planos para hoje?`,
             WELCOME_REPROMPT_MSG: `Meu plano para hoje é ficar aqui na minha caixinha. Quais são os seus?`,
             WELCOME_BACK_MSG: `Hm deixa eu ver aqui... Parece que suas metas para hoje são {{goals}}. Você quer adicionar outras ou remover alguma delas?`,
+            WELCOME_BACK_REPROMPT_MSG: `Você quer adicionar ou remover alguma meta para hoje?`,
             NO_GOALS_MSG: `Parece que você não tem metas para hoje!`,
             REGISTER_GOALS_MSG: `Obrigada, eu vou lembrar que suas metas para hoje são {{goals}}.`,
             HELP_MSG: `Você pode me dizer quais são as atividades que quer realizar hoje. Eu vou guardar e você pode verificar comigo ao longo do dia!`,
