6184bbe9c6a363d936768aacb2bc9b10a5b55b5c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-05-23 23:22:29 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -258,7 +258,7 @@ const CancelAndStopIntentHandler = {
                 || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speakOutput = handlerInput.t('GOODBYE_MSG' + randomIndex(4));
+        const speakOutput = handlerInput.t('GOODBYE_MSG' + randomIndex(5));
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
